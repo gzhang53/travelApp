995d586df8316ea277f8e4f4566897a6080bd4db
Jimmy Zhang <gzhang53@uwo.ca>
2020-08-25 11:56:55 -0400

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+import {submit} from "./js/app.js"
+import {getCoordinates} from "./js/getCoordinates.js"
+
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+//document.addEventListener('DOMContentLoaded ', () => {    
+//     document.getElementById("submit").addEventListener("click", handleSubmit);
+// })
+
+export{
+    submit,
+    getCoordinates,
+};
